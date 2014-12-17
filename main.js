93f3945f8beb1de819ab89f73e1c7ebd3f9cb75b
Kartik Talwar <hi@kartikt.com>
2014-12-16 21:29:40 -0800

    Added gmail.js latest version disclaimer

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ function refresh(f) {
 
 
 var main = function(){
+  // NOTE: Always use the latest version of gmail.js from
+  // https://github.com/KartikTalwar/gmail.js
   gmail = new Gmail();
   console.log('Hello,', gmail.get.user_email())
 }
